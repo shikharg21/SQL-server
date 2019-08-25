0e7dce359e4b60e31732bbadf2edeba00fe5c709
Shikhar <shikharg21@gmail.com>
2019-08-25 11:49:42 +0530

    db added
    Signed-off-by: Shikhar <shikharg21@gmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const db = require('./db');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -8,19 +9,31 @@ app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-    res.render('persons', {
-        persons: [
-            { name: 'Abc', age: 21, city: 'delhi' },
-            { name: 'def', age: 23, city: 'madras' },
-            { name: 'ghi', age: 17, city: 'mumbai' }
-        ]
-    })
+
+    db.getAllPersons()
+        .then((persons) => {
+            res.render('persons', { persons })
+        })
+        .catch((err) => {
+            res.send(err)
+        })
+
 })
 
 app.get('/add', (req, res) => {
     res.render('persons_add')
 })
 
+app.post('/add', (req, res) => {
+    db.addnewPerson(req.body.name,req.body.age,req.body.city)
+    .then(()=>{
+          res.redirect('/')
+    })
+    .catch((err)=>{
+          res.send(err)
+    })
+
+})
 
 
 app.listen(3434, () => {
